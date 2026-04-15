67cbc8d40564e4d8c35df5346bff95db2978e1d5
zhangxinhui02 <i@miku.team>
2026-04-15 14:03:38 +0800

    Temporary commit

--- backend/src/chip-testing-flowcard-generator-backend/api_router.py
+++ backend/src/chip-testing-flowcard-generator-backend/api_router.py
@@ -1,11 +1,13 @@
 import logging
 import uvicorn
-from fastapi import FastAPI
+from typing import Literal
+from fastapi import FastAPI, UploadFile, File
 from contextlib import asynccontextmanager
 
-from config import common_config
+from config import const_config, common_config
 
 logger = logging.getLogger(__file__)
+prefix = const_config.api_prefix
 
 @asynccontextmanager
 async def lifespan(_):
@@ -16,9 +18,20 @@ async def lifespan(_):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/api")
+@app.get(f"{prefix}/")
 def root():
     return {"message": "Hello World"}
+
+
+# 知识库
+@app.get(f'{prefix}/docs')
+def get_docs():
+    """获取所有的文档信息"""
+    pass
+
+@app.post(f'{prefix}/docs')
+def create_doc(title: str, file_type: Literal['image', 'pdf', 'markdown', 'txt'], file: UploadFile = File(...)):
+    """创建文档"""
 
 
 def run():
--- backend/src/chip-testing-flowcard-generator-backend/component/rag.py
+++ backend/src/chip-testing-flowcard-generator-backend/component/rag.py
@@ -1,19 +1,97 @@
-from langchain_milvus import Milvus
+import os
+import shutil
+import logging
+from typing import Literal
+from pydantic import SecretStr
+from pymilvus import MilvusClient
+from langchain_ollama import OllamaEmbeddings
 
-from config import llm_model_config, embedding_model_config, reranking_model_config, milvus_config
-from client.milvus_client import MilvusClient
+from config import const_config, embedding_model_config, reranking_model_config, milvus_config
+from schema.milvus_collection import docs_schema
+import util
 
-milvus_client: Milvus | None = None
+logger = logging.getLogger(__name__)
+is_initialized = False
+storage_dir = os.path.join(const_config.storage_dir, 'rag')
+temp_dir = os.path.join(const_config.temp_dir, 'rag')
 
-def init_knowledge_db():
-    """初始化RAG知识库"""
-    # 检查Milvus数据库连接
+milvus_client: MilvusClient | None = None
+embeddings_model = OllamaEmbeddings(
+    base_url=embedding_model_config.base_url,
+    model=embedding_model_config.model
+)
+
+
+def __init_milvus_database():
+    """初始化Milvus数据库"""
+    # 连接Milvus
+    global milvus_client
+    logger.info('Connecting to Milvus server...')
+    logger.debug('Milvus Server Info:')
+    logger.debug('\tURL: %s', milvus_config.url)
+    logger.debug('\tDatabase: %s', milvus_config.database)
+    milvus_client = MilvusClient(
+        uri=milvus_config.url,
+        token=milvus_config.token,
+        db_name=milvus_config.database,
+        timeout=30
+    )
+    logger.info('Connected to Milvus server')
+
+    # 检查数据库
+    logger.info('Initializing database `%s`...', milvus_config.database)
+    databases = milvus_client.list_databases()
+    if milvus_config.database not in databases:
+        logger.debug('\tCreating database `%s`...', milvus_config.database)
+        milvus_client.create_database(db_name=milvus_config.database)
+        logger.debug('\tDatabase `%s` created.', milvus_config.database)
+
+    # 检查Collections
+    collection_schemas = {
+        'docs': docs_schema,
+        # 'users': user_schema  # todo 多用户系统
+    }
+    collections = milvus_client.list_collections()
+    for collection_name in collection_schemas:
+        if collection_name not in collections:
+            logger.debug('\tCreating collection `%s`...', collection_name)
+            milvus_client.create_collection(
+                collection_name=collection_name,
+                schema=collection_schemas[collection_name]
+            )
+            logger.debug('\tCollection `%s` created.', collection_name)
+
+    logger.info('Database `%s` initialized.', milvus_config.database)
+
+
+async def __vectorize_chunks(chunk: str) -> list[float]:
+    """
+    向量化单个chunk
+    :param chunk: chunk文本
+    :return: 向量
+    """
+    return await embeddings_model.aembed_query(chunk)
+
+
+async def vectorize_doc_to_db(
+        doc_path: str,
+        doc_type: Literal['image', 'pdf', 'markdown', 'txt']
+) -> str:
+    """
+    向量化文档内容并保存至向量数据库
+    :param doc_path: 文档路径
+    :param doc_type: 文档类型
+    :return: 文档ID
+    """
     pass
-    # 检查数据库是否存在
-    pass
-    # 检查元数据表是否存在
-    # doc
-    # user
-    #
-    # 遍历本地持久化文档，检查对应的文档知识库是否存在，如果不存在则创建
-    pass
+    # file_id =
+    # shutil.copyfile(doc_path, os.path.join(temp_dir, ))
+    # if doc_type == 'image':
+
+
+
+if not is_initialized:
+    logger.info('Initializing Milvus client...')
+    os.makedirs(os.path.join(const_config.storage_dir, 'rag'), exist_ok=True)
+    os.makedirs(os.path.join(const_config.temp_dir, 'rag'), exist_ok=True)
+    __init_milvus_database()
