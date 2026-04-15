16191cce84a76b4d08399bd15fb4504bdbfe9895
zhangxinhui02 <i@miku.team>
2026-04-16 02:21:20 +0800

    Remove generate_doc_id_from_content module

--- backend/src/chip-testing-flowcard-generator-backend/util/__init__.py
+++ backend/src/chip-testing-flowcard-generator-backend/util/__init__.py
@@ -2,6 +2,6 @@ from .init_logging import init_logging
 from .image_to_pdf import image_to_pdf
 from .pdf_to_markdown import pdf_to_markdown
 from .slice_markdown_to_chunks import slice_markdown_to_chunks
-from .generate_doc_id_from_content import generate_doc_id_from_content
+from .generate_unique_id import generate_unique_id
 
-__all__ = [init_logging, image_to_pdf, pdf_to_markdown, slice_markdown_to_chunks, generate_doc_id_from_content]
+__all__ = [init_logging, image_to_pdf, pdf_to_markdown, slice_markdown_to_chunks, generate_unique_id]
