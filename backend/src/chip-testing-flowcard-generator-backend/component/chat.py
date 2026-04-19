67a91873967543f38c8abd9252b208bdef781d39
zhangxinhui02 <i@miku.team>
2026-04-19 23:49:54 +0800

    Add chat routes

--- backend/src/chip-testing-flowcard-generator-backend/component/chat.py
+++ backend/src/chip-testing-flowcard-generator-backend/component/chat.py
@@ -32,10 +32,10 @@ prompts = {}
 
 if not __is_initialized:
     with open(os.path.join(chat_storage_dir, 'metadata.json'), 'r', encoding='utf-8') as _f:
-        cached_chat_metadata = json.load(_f)
+        cached_chat_metadata: dict = json.load(_f)
     for _title in ['common-pre-prompt', 'chat-with-docs', 'chat', 'generate-chat-title']:
         with open(os.path.join(prompts_dir, f'{_title}.md'), 'r', encoding='utf-8') as _f:
-            prompts[_title] = _f.read()
+            prompts[_title]: str= _f.read()
     __is_initialized = True
 
 
@@ -57,33 +57,52 @@ async def __delete_chat_metadata(chat_id: str):
 
 
 async def __update_chat_metadata(chat_id: str, title: str):
-    """删除指定chat_id的元信息"""
+    """修改指定chat_id的元信息"""
     if chat_id in cached_chat_metadata:
         cached_chat_metadata['title'] = title
         async with aiofiles.open(os.path.join(chat_storage_dir, 'chats.json'), 'w', encoding='utf-8') as f:
             await f.write(json.dumps(cached_chat_metadata, ensure_ascii=False, indent=4))
 
 
-async def __dump_persistent_history(chat_id: str, history: list[BaseMessage]):
-    """将聊天历史保存到持久化存储"""
-    json_history = []
-    for message in history:
+def convert_messages_to_dicts(messages: list[BaseMessage]) -> list[dict]:
+    """将BaseMessage对象列表转换为dict列表"""
+    results = []
+    for message in messages:
         if isinstance(message, SystemMessage):
-            json_history.append({
+            results.append({
                 'type': 'SystemMessage',
                 'content': message.content
             })
         elif isinstance(message, HumanMessage):
-            json_history.append({
+            results.append({
                 'type': 'HumanMessage',
                 'content': message.content
             })
         else:  # AIMessage
-            json_history.append({
+            results.append({
                 'type': 'AIMessage',
                 'content': message.content
             })
-    content = json.dumps(json_history, ensure_ascii=False, indent=4)
+    return results
+
+
+def convert_dicts_to_messages(dicts: list[dict]) -> list[BaseMessage]:
+    """将dict列表转换为BaseMessage对象列表"""
+    results = []
+    for message in dicts:
+        if message['type'] == 'SystemMessage':
+            results.append(SystemMessage(message['content']))
+        elif message['type'] == 'HumanMessage':
+            results.append(HumanMessage(message['content']))
+        else:  # AIMessage
+            results.append(AIMessage(message['content']))
+    return results
+
+
+async def __dump_persistent_history(chat_id: str, history: list[BaseMessage]):
+    """将聊天历史保存到持久化存储"""
+    _history = convert_messages_to_dicts(history)
+    content = json.dumps(_history, ensure_ascii=False, indent=4)
     async with aiofiles.open(os.path.join(chat_storage_dir, f'{chat_id}.json'), 'w', encoding='utf-8') as f:
         await f.write(content)
 
@@ -95,14 +114,7 @@ async def __load_persistent_history(chat_id: str) -> list[BaseMessage]:
     async with aiofiles.open(os.path.join(chat_storage_dir, f'{chat_id}.json'), 'r', encoding='utf-8') as f:
         content = await f.read()
     json_history = json.loads(content)
-    history = []
-    for message in json_history:
-        if message['type'] == 'SystemMessage':
-            history.append(SystemMessage(message['content']))
-        elif message['type'] == 'HumanMessage':
-            history.append(HumanMessage(message['content']))
-        else:  # AIMessage
-            history.append(AIMessage(message['content']))
+    history = convert_dicts_to_messages(json_history)
     return history
 
 
@@ -114,7 +126,7 @@ async def __delete_timeout_chat_history_cache():
             del cached_chat_history[chat_id]
 
 
-async def __dump_history(chat_id: str, history: list[BaseMessage]):
+async def dump_history(chat_id: str, history: list[BaseMessage]):
     """保存聊天历史到缓存和持久化存储"""
     cached_chat_history[chat_id] = history
     await __dump_persistent_history(chat_id, history)
@@ -122,7 +134,7 @@ async def __dump_history(chat_id: str, history: list[BaseMessage]):
     await __delete_timeout_chat_history_cache()
 
 
-async def __load_history(chat_id: str) -> list[BaseMessage]:
+async def load_history(chat_id: str) -> list[BaseMessage]:
     """读取聊天历史，优先读缓存，缓存不存在时从持久化存储中读取"""
     if cached_chat_history.get(chat_id, None) is None:
         cached_chat_history[chat_id] = await __load_persistent_history(chat_id)
@@ -143,39 +155,47 @@ async def __generate_chat_title(message: str) -> str:
     return chat_title.title
 
 
-async def create_chat() -> str:
-    """新建一个聊天，返回聊天ID"""
-    chat_id = generate_unique_id(unique_checking_sequence=cached_chat_metadata.keys())
-    await __add_chat_metadata(chat_id, '')
-    return chat_id
+def get_all_chats() -> list[dict[str, str]]:
+    """获取所有聊天的id和标题"""
+    return [
+        {
+            'id': _chat_id,
+            'title': cached_chat_metadata[_chat_id]['title']
+        }
+        for _chat_id in cached_chat_metadata
+    ]
 
 
-async def delete_chat(chat_id: str):
+async def delete_chat(chat_id: str) -> bool:
     """从缓存和持久化存储中删除聊天"""
     await __delete_chat_metadata(chat_id)
     del cached_chat_history[chat_id]
     del chat_history_timeout[chat_id]
     os.remove(os.path.join(chat_storage_dir, f'{chat_id}.json'))
+    return True
 
 
-async def update_chat_title(chat_id: str, title: str):
+async def update_chat_title(chat_id: str, title: str) -> bool:
     """更新聊天的标题"""
     await __update_chat_metadata(chat_id, title)
+    return True
 
 
 async def chat(
-    chat_id: str,
     message: str,
     using_doc_ids: Sequence[str] = (),
     k: int = 10,
-    reranking_k: int | None = None
+    reranking_k: int | None = None,
+    chat_id: str | None = None
 ) -> str:
     """与大语言模型交流，可选多个文档用于RAG"""
-    history = await __load_history(chat_id)
-    if len(history) == 0:
-        history.append(SystemMessage(prompts['common-pre-prompt']))
+    if chat_id is None:  # 新聊天
+        chat_id: str = generate_unique_id(unique_checking_sequence=list(cached_chat_metadata.keys()))
         title = await __generate_chat_title(message)
-        await update_chat_title(chat_id, title)
+        await __add_chat_metadata(chat_id, title)
+        history: list[BaseMessage] = [SystemMessage(prompts['common-pre-prompt'])]
+    else:
+        history: list[BaseMessage] = await load_history(chat_id)
 
     if len(using_doc_ids) > 0:
         results = await knowledge.query_from_docs(message, using_doc_ids, k=k, reranking_k=reranking_k)
@@ -189,5 +209,5 @@ async def chat(
 
     response: AIMessage = await llm_client.ainvoke(history)
     history.append(response)
-    await __dump_history(chat_id, history)
+    await dump_history(chat_id, history)
     return response.content
--- backend/src/chip-testing-flowcard-generator-backend/routes/chat.py
+++ backend/src/chip-testing-flowcard-generator-backend/routes/chat.py
@@ -1,33 +1,63 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix='/qa', tags=['qa'])
+from component import chat
+import util
+
+router = APIRouter(prefix='/chats', tags=['qa'])
 
 
-@router.get('/chats')
-def get_chats():
-    """获取所有聊天列表"""
-    pass
+@router.get('/', response_model=list[dict[str, str]])
+async def get_chats() -> list[dict[str, str]]:
+    """获取所有聊天的ID和标题"""
+    return chat.get_all_chats()
 
 
-@router.get('/chats/{chat_id}')
-def get_chat(chat_id: str):
+@router.get('/{chat_id}', response_model=list[dict[str, str]])
+async def get_chat(chat_id: str) -> list[dict[str, str]]:
     """获取单个聊天的全部对话历史"""
-    pass
+    history = await chat.load_history(chat_id)
+    return chat.convert_messages_to_dicts(history)
 
 
-@router.delete('/chats/{chat_id}')
-def delete_chat(chat_id: str):
+@router.delete('/{chat_id}', response_model=bool)
+async def delete_chat(chat_id: str) -> bool:
     """删除指定的聊天"""
-    pass
+    return await chat.delete_chat(chat_id)
 
 
-@router.post('/chats')
-def create_chat() -> str:
-    """开启新聊天"""
-    pass
+@router.post('/')
+async def create_chat() -> str:
+    """开启一个新聊天，返回chat_id"""
+    return util.generate_unique_id(unique_checking_sequence=[_chat['id'] for _chat in chat.get_all_chats()])
 
 
-@router.post('/chats/{chat_id}/chat')
-def chat(chat_id: str, using_docs: list[str]):
-    """在指定聊天中发送信息并接收响应"""
-    pass
+@router.post('/{chat_id}/chat')
+async def chat(
+        chat_id: str,
+        message: str,
+        using_docs: list[str],
+        k: int = 10,
+        reranking_k: int | None = None
+):
+    """
+    与大语言模型交流。
+    :param chat_id: 聊天ID。
+    :param message: 用户发出的单条消息。
+    :param using_docs: 要使用的知识库文档ID的列表，用于RAG检索。如果为空，则不使用RAG。
+    :param k: RAG在每个文档中查找k条语义最相关的内容。
+    :param reranking_k: RAG在每个文档中查找reranking_k条语义最相关的内容，重排序后取k条结果。如果为None，则不使用重排序（加快响应速度）。
+    :return: 大语言模型响应。
+    """
+    return await chat.chat(
+        message,
+        using_docs,
+        k,
+        reranking_k,
+        chat_id
+    )
+
+
+@router.put('/{chat_id}')
+async def update_chat_title(chat_id: str, title: str):
+    """修改聊天的标题"""
+    return await chat.update_chat_title(chat_id, title)
